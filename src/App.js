bf390c3aa4fde3865bffc3bc7178fe87a45ec26b
Dalth F <daleth.morales@myuvm.edu.mx>
2022-09-11 21:13:29 -0500

    Primer Comit Inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './conteiners/Home';
+import Error404 from './conteiners/errors/Error404';;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Error404 */}
+          <Route path="*" element={<Error404 />} />
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
-
-export default App;
+export default App
